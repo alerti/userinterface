fbacf085eeee9ed6d41825aa3d7c7b897d14160b
alerti <mukunzialex47@gmail.com>
2020-09-08 12:42:11 -0400

    Updates

---
+++ precache-manifest.a574a2a8b23eb2d017faa868e403f5e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee51bab985510faab5a7342ef14df8d5",
+    "url": "/e-pesa/index.html"
+  },
+  {
+    "revision": "c9a8b857adc81ae5e6b8",
+    "url": "/e-pesa/static/css/main.ee5c9ba7.chunk.css"
+  },
+  {
+    "revision": "9ddb024aad8e9156e17c",
+    "url": "/e-pesa/static/js/2.96fe2d77.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/e-pesa/static/js/2.96fe2d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a8b857adc81ae5e6b8",
+    "url": "/e-pesa/static/js/main.d71d10e3.chunk.js"
+  },
+  {
+    "revision": "a0e797a07e66a470a07a",
+    "url": "/e-pesa/static/js/runtime-main.9f7f8cdb.js"
+  },
+  {
+    "revision": "c9f6272a0d73683539610aa60106b6e9",
+    "url": "/e-pesa/static/media/epsa.c9f6272a.svg"
+  },
+  {
+    "revision": "34887f17165b6a39c9771d8fa7f73e95",
+    "url": "/e-pesa/static/media/phone.34887f17.png"
+  }
+]);
